8dc02c6b89116f172e6149ef1b46c5cfee3d477c
McCouman <support@wikibyte.org>
2013-10-28 22:10:32 +0100

    2011-12-01 T 19:59:37.000000Z

---
+++ ButtonFamily/ButtonFamily.i18n.php
@@ -0,0 +1,37 @@
+<?php
+/***************************************************************
+#
+# Author:		Michael McCouman jr.
+# Revision:     09-Apr-2006 / 22:25	
+# Release: 		17.x-18.0
+# Copyright (c) 2010 Wikiunity - http://www.wikiunity.com
+#
+****************************************************************/
+
+$messages = array();
+ 
+
+$messages['de'] = array(
+	'buttons-desc' => 'erstellt eine globale Button Familie, zum schnellen bearbeiten einer Wikiseite in Wikiunity',
+	'buttons-toc' => 'TOC einfuegen',
+	'buttons-br' => 'Zeilenumbruch einfuegen',
+	'buttons-pre' => 'Quelltext einfuegen',
+	'buttons-pre2' => 'Hier Quelltext einfuegen',
+	'buttons-code' => 'Code einfuegen',
+	'buttons-code2' => 'Hier Code einfuegen',
+	'buttons-strike' => 'Durchstreichen',
+	'buttons-strike2' => 'Durchgestrichener Text',	
+	'buttons-small' => 'Kleinere Schrift',
+	'buttons-small2' => 'Kleiner Text',
+	'buttons-big' => 'Groessere Schrift',
+	'buttons-big2' => 'Grosser Text',
+	'buttons-redirect' => 'Umleitung',
+	'buttons-redirect2' => 'Ziel eingeben',
+	'buttons-category' => 'Kategorie',
+	'buttons-category2' => 'Deine Kategorie',
+	'buttons-gallery' => 'Bildergallerie',
+	'buttons-gallery2' => 'Image:Bild1.jpg|Ueberschrift\\nImage:Bild2.jpg|Ueberschrift2\\n',
+	'buttons-table' => 'Tabelle einfuegen',
+	'buttons-table2' => '! Ueberschrift 1\\n! Ueberschrift 2\\n! Ueberschrift 3\\n|-\\n| Zeile 1, Zelle 1\\n| Zeile 1, Zelle 2\\n| Zeile 1, Zelle 3\\n|-\\n| Zeile 2, Zelle 1\\n| Zeile 2, Zelle 2\\n| Zeile 2, Zelle 3',
+);
+
